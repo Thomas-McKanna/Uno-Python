032433cf403441cd0010f83159e4b34c3920477c
Austin Wall <austin@mst.edu>
2019-08-29 07:35:58 -0500

    Create player class

--- player.py
+++ player.py
@@ -1,3 +1,16 @@
+from cards import Hand, Deck
 class Player: 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, name, deck: Deck):
+        self.name = name
+        self.hand = Hand(deck)
+
+
+    def draw(self, number):
+        self.hand.draw(number)
+
+    def playCard(self, idx):
+        """
+        Attempts to play a card from the player's hand at index idx
+        """
+        print(self.hand.deck.getDiscard())
+        print(self.hand.cards[idx])
