b46c3bb39eacad295f63cb6769a561288fe4e6da
Austin Wall <austin@mst.edu>
2019-09-17 07:44:27 -0500

    Rename card tests to prevent collision

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -4,11 +4,13 @@ from cards import Card, Hand, Deck, ComplexEncoder
 
 
 
-def test_str():
+# Test Card class
+
+def test_card_str():
     card = Card(1, "4", "Red")
     assert card.__str__() == "Red 4"
 
-def test_repr():
+def test_card_repr():
     card = Card(1, "4", "Red")
     assert card.__repr__() == "Red 4"
 
@@ -18,10 +20,10 @@ def test_repr():
  (Card(1, "2", "Red"), Card(2, "4", "Blue"), False), # Test no match
  (Card(1, "4", "Red"), Card(2, "4", "Red"), True)] # Test both match
 )
-def test_match(card1, card2, output):
+def test_card_match(card1, card2, output):
     assert card1.match(card2) == output
 
-def test_reprJSON():
+def test_card_reprJSON():
     output = {"id": 1, "value": "4", "color": "Red"}
     card = Card(1, "4", "Red")
     assert card.reprJSON() == output
